7cb8e750cba3b63e460716f2423e0f782db0bb2e
mpsingh17 <mpsingh17@outlook.co.nz>
2020-06-11 22:44:28 +1200

    map claim types correctly.

--- CompanyEmployees.Client/Startup.cs
+++ CompanyEmployees.Client/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -20,6 +21,7 @@ namespace CompanyEmployees.Client
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
         }
 
         public IConfiguration Configuration { get; }
