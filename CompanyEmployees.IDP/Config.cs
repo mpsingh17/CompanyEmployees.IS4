bf7b3b21655ad69940599c5ef60a028a3f27acd2
mpsingh17 <mpsingh17@outlook.co.nz>
2020-06-11 20:56:04 +1200

    initial commit including solution.

---
+++ CompanyEmployees.IDP/Config.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace CompanyEmployees.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            { };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            {
+                new Client
+                {
+                    ClientName = "CompanyEmployeeClient",
+                    ClientId = "companyemployeeclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string> { "https://localhost:5010/signin-oidc" },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("CompanyEmployeeClientSecret".Sha512())
+                    },
+                    //RequireConsent = false
+                }
+            };
+        
+    }
+}
